93d1c9c561ba7948a8793663b931e67842716c39
SONY <SONY@DESKTOP-PUTEV6N>
2022-06-19 22:08:22 +0530

    By Miss

---
+++ GITHUBP1/src/Task1.java
@@ -0,0 +1,14 @@
+
+public class Task1 {
+	public void m1() {
+		System.out.println("M1");
+	}
+	public void m2() {
+		System.out.println("M1");
+	}
+	
+public static void main(String[] args) {
+	System.out.println(" Main ");
+	
+}
+}
